f726e712af82a001446b0dccee322c9c92d74cc2
Serafima Nazarova <sima.nazarova@gmail.com>
2025-01-24 18:13:18 +0300

    исправила проблему со сборкой билда

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  pageExtensions: ['page.tsx', 'page.ts', 'ts'],
 };
 
 export default nextConfig;
